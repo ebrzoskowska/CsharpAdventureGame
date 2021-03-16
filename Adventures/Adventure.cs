8d1b23bcb2634b4edee55f5e8b460043134221ea
Emilia Brzoskowska <79784263+ebrzoskowska@users.noreply.github.com>
2021-03-16 11:42:09 +0000

    Update

--- Adventures/Adventure.cs
+++ Adventures/Adventure.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,15 +7,19 @@ namespace AdventureGame.Adventures
     public class Adventure
     {
         public string Title { get; set; }
-
         public string gameTitleDescription { get; set; }
+        public string firstChapterDescription { get; set; }
+        public string secondChapterDescription { get; set; }
+        public string secondChapterRiddle { get; set; }
+        public string secondChapterPropAnswer { get; set; }
+        public string thirdChapterDescription { get; set; }
+        public string fourthChapterDescription { get; set; }
+        public string fifthChapterDescription { get; set; }
+        public string playerIsDead { get; set; }
 
         public Adventure()
         {
 
         }
-
-       
-
     }
 }
