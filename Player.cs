3395d77725910ae40943b242e350744b216a352c
Emilia Brzoskowska <79784263+ebrzoskowska@users.noreply.github.com>
2021-03-16 10:31:34 +0000

    update

--- Player.cs
+++ Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -16,7 +16,8 @@ namespace AdventureGame
         public static void playerStats()
         {
             Console.Clear();
-            Console.WriteLine("        PLAYER STATS        ");
+			Console.WriteLine("");
+			Console.WriteLine("        PLAYER STATS        ");
             Console.WriteLine("");
             Console.WriteLine("****************************");
             Console.WriteLine("");
@@ -25,11 +26,129 @@ namespace AdventureGame
             Console.WriteLine("");
             Console.WriteLine("****************************");
             Console.ReadKey();
+            Console.Clear();
+			if (Program.currentPlayer.health <= 0)
+            {
+				Program.gameOver();
+            }
         }
 
         public static void newPlayer()
         {
-//
-        }
+
+			string characterChoice;
+			string choiceConfirmation;
+			int correct = 0;
+
+			do
+			{
+
+				Console.Clear();
+				Console.WriteLine("");
+				Console.WriteLine("Please choose character type as below: ");
+				Console.WriteLine("");
+				Console.WriteLine("(M)age");
+				Console.WriteLine("(W)arrior");
+				Console.WriteLine("(R)ogue");
+				Console.WriteLine("");
+				Console.WriteLine("Choice: ");
+				characterChoice = Console.ReadLine().ToLower();
+				Console.Clear();
+
+				switch (characterChoice)
+				{
+					case "m":
+					case "mage":
+						Console.WriteLine("");
+						Console.WriteLine("This character is a spell-caster that uses fire and shadow magic against their enemies.");
+						Console.WriteLine("Is this the character you wish to play?");
+						Console.WriteLine("");
+						Console.WriteLine(" Y / N ");
+						Console.WriteLine("");
+						Console.WriteLine("Choice: ");
+						choiceConfirmation = Console.ReadLine().ToLower();
+
+						switch (choiceConfirmation)
+						{
+							case "yes":
+							case "y":
+								correct = 1;
+								break;
+							case "no":
+							case "n":
+								correct = 0;
+								break;
+							default:
+								Console.WriteLine("Just try again...");
+								Console.WriteLine("press ENTER to continue");
+								break;
+						}
+
+						break;
+					case "w":
+					case "warrior":
+						Console.WriteLine("");
+						Console.WriteLine("This character has the widest array of weapons to choose from in the game,");
+						Console.WriteLine("being able to equip every weapon type in the game except for wands.");
+						Console.WriteLine("Is this the character you wish to play?");
+						Console.WriteLine("");
+						Console.WriteLine(" Y / N ");
+						Console.WriteLine("");
+						Console.WriteLine("Choice: ");
+						choiceConfirmation = Console.ReadLine().ToLower();
+
+						switch (choiceConfirmation)
+						{
+							case "yes":
+							case "y":
+								correct = 1;
+								break;
+							case "no":
+							case "n":
+								correct = 0;
+								break;
+							default:
+								Console.WriteLine("Just try again...");
+								Console.WriteLine("press ENTER to continue");
+								break;
+						}
+
+						break;
+					case "r":
+					case "rouge":
+						Console.WriteLine("");
+						Console.WriteLine("A rogue is a versatile character, capable of sneaky combat and nimble tricks.");
+						Console.WriteLine("The rogue is stealthy and dexterous, capable of finding and disarming traps and picking locks.");
+						Console.WriteLine("Is this the character you wish to play?");
+						Console.WriteLine("");
+						Console.WriteLine(" Y / N ");
+						Console.WriteLine("");
+						Console.WriteLine("Choice: ");
+						choiceConfirmation = Console.ReadLine().ToLower();
+
+						switch (choiceConfirmation)
+						{
+							case "yes":
+							case "y":
+								correct = 1;
+								break;
+							case "no":
+							case "n":
+								correct = 0;
+								break;
+							default:
+								Console.WriteLine("Just try again...");
+								Console.WriteLine("press ENTER to continue");
+								break;
+						}
+
+						break;
+					default:
+						Console.WriteLine("Just try again...");
+						Console.WriteLine("press ENTER to continue");
+						break;
+				}
+			} while (correct == 0);
+		}
     }
 }
